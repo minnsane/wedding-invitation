2e7bf00a60ae3598588bba63aba8c79a8cfae9da
minnsane <minnsane@daum.net>
2022-06-29 01:03:42 +0900

    chore: main 컴포넌트 home 컴포넌트로 네이밍 변경

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import { Swiper, SwiperSlide } from "swiper/svelte";
   import Timeline from "./components/Timeline.svelte";
-  import Main from "./components/Main.svelte";
+  import Home from "./components/Home.svelte";
 
   import "swiper/css";
 </script>
 
 <main>
   <Swiper slidesPerView={1} direction={"vertical"}>
-    <SwiperSlide><Main /></SwiperSlide>
+    <SwiperSlide><Home /></SwiperSlide>
     <SwiperSlide><Timeline /></SwiperSlide>
   </Swiper>
 </main>
