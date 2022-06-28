938a02673368c4f61eb7652198d33ac929fcb7d6
minnsane <minnsane@daum.net>
2022-06-29 00:43:37 +0900

    feat: 메인화면 컴포넌트 생성

---
+++ src/components/Main.svelte
@@ -0,0 +1,60 @@
+<script>
+  const [groom, bride] = ["이성화", "김민정"];
+  const connector = "+";
+  const details = ["22년 9월 17일 12시 40분", "베뉴지웨딩 네이처홀"];
+</script>
+
+<div class="main">
+  <div class="description">
+    <h1 class="title">
+      <span>{groom}</span>
+      <span class="connector">{connector}</span>
+      <span>{bride}</span>
+    </h1>
+    {#each details as detail}
+      <span class="detail">{detail}</span>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .main {
+    background-image: url("../assets/photo01.jpg");
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+  }
+
+  .description {
+    position: absolute;
+    top: 15vh;
+    right: 10vw;
+    width: 100%;
+    height: 100%;
+    text-align: right;
+  }
+
+  .title {
+    font-weight: 300;
+    font-size: 30px;
+    color: #313e40;
+    margin-bottom: 12px;
+  }
+
+  .title span {
+    display: inline-block;
+  }
+
+  .connector {
+    opacity: 0.6;
+    font-size: 20px;
+    line-height: 20px;
+  }
+
+  .detail {
+    opacity: 0.6;
+    font-size: 18px;
+    display: block;
+    line-height: 22px;
+  }
+</style>
