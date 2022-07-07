f031699f6bfbe4d1f618427164c19e167494db05
minnsane <minnsane@daum.net>
2022-07-07 16:48:36 +0900

    feat: temp 청첩장 작업

---
+++ src/components/temp/Loading.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<div class="wrapper">
+  <img src="/src/assets/inprogress.gif" alt="in progress" />
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: inherit;
+  }
+</style>
