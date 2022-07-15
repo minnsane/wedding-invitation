020784a23aff4c24a7cedfe8615ad3d95a0d02ea
minnsane <minnsane@daum.net>
2022-07-16 02:00:48 +0900

    feat: PC alert 안내문구

--- src/App.svelte
+++ src/App.svelte
@@ -3,22 +3,36 @@
   import Home from "./components/Home.svelte";
   import Invitation from "./components/Invitation.svelte";
   import "swiper/css";
-  import { onMount } from "svelte";
+  import { afterUpdate, onMount } from "svelte";
   import { getMain } from "./lib/api";
   import { response } from "./shared/app.store";
+
+  const MAX_MOBILE_SCREEN_WIDTH = 512;
 
   onMount(async () => {
     const mainData = await getMain();
     response.set(mainData);
   });
+
+  let innerWidth;
+
+  afterUpdate(() => {
+    if (innerWidth >= MAX_MOBILE_SCREEN_WIDTH) {
+      alert("현재는 모바일 버전만 지원하고 있어요. 조금만 기다려주세요!");
+    }
+  });
 </script>
 
+<svelte:window bind:innerWidth />
+
 <main>
-  <Swiper slidesPerView={1} direction={"vertical"}>
-    <SwiperSlide><Home /></SwiperSlide>
-    <SwiperSlide><Invitation /></SwiperSlide>
-    <!--
+  {#if innerWidth < MAX_MOBILE_SCREEN_WIDTH}
+    <Swiper slidesPerView={1} direction={"vertical"}>
+      <SwiperSlide><Home /></SwiperSlide>
+      <SwiperSlide><Invitation /></SwiperSlide>
+      <!--
     <SwiperSlide><Timeline /></SwiperSlide>
     -->
-  </Swiper>
+    </Swiper>
+  {/if}
 </main>
