135a81e1964cee9b553c8bf78b246453f6d4253a
minnsane <minnsane@daum.net>
2022-07-15 23:32:56 +0900

    feat: 앱 로드 시작점에 메인 데이터 fetch

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
   import { Swiper, SwiperSlide } from "swiper/svelte";
-  // import Home from "./components/Home.svelte";
-  import Home from "./components/temp/Home.svelte";
+  import Home from "./components/Home.svelte";
+  import Invitation from "./components/Invitation.svelte";
   import "swiper/css";
-  import Loading from "./components/temp/Loading.svelte";
+  import { onMount } from "svelte";
+  import { getMain } from "./lib/api";
+  import { response } from "./shared/app.store";
+
+  onMount(async () => {
+    const mainData = await getMain();
+    response.set(mainData);
+  });
 </script>
 
 <main>
   <Swiper slidesPerView={1} direction={"vertical"}>
     <SwiperSlide><Home /></SwiperSlide>
-    <SwiperSlide><Loading /></SwiperSlide>
-    <!--
     <SwiperSlide><Invitation /></SwiperSlide>
+    <!--
     <SwiperSlide><Timeline /></SwiperSlide>
     -->
   </Swiper>
