40d1bf8117bc3a8575a14834501eb4bb9d755e75
minnsane <minnsane@daum.net>
2022-07-18 13:45:14 +0900

    fix: 스토어 맵 구독 취소 추가

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -1,56 +1,58 @@
 <script lang="ts">
-  import { beforeUpdate } from "svelte";
+  import { beforeUpdate, onDestroy } from "svelte";
   import { map } from "../shared/app.store";
   import { mdiArrowRightBold } from "@mdi/js";
   import { Icon } from "svelte-chota";
 
   let mapContainer;
+  let unsubscribeMap;
 
-  beforeUpdate(() =>
-    map.subscribe((mapValue) => {
-      if (window.kakao || mapValue === null) {
-        return;
-      }
+  beforeUpdate(
+    () =>
+      (unsubscribeMap = map.subscribe((mapValue) => {
+        if (window.kakao || mapValue === null) {
+          return;
+        }
 
-      const mapScript = document.createElement("script");
+        const mapScript = document.createElement("script");
 
-      mapScript.async = true;
-      mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${mapValue.key}&autoload=false`;
+        mapScript.async = true;
+        mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${mapValue.key}&autoload=false`;
 
-      document.head.appendChild(mapScript);
+        document.head.appendChild(mapScript);
 
-      const onLoadKakaoMap = (): void => {
-        window.kakao.maps.load(() => {
-          const options = {
-            level: 5,
-            center: new window.kakao.maps.LatLng(
+        const onLoadKakaoMap = (): void => {
+          window.kakao.maps.load(() => {
+            const options = {
+              level: 5,
+              center: new window.kakao.maps.LatLng(
+                mapValue.position.lat,
+                mapValue.position.lng
+              ),
+            };
+            const kakaoMap = new window.kakao.maps.Map(mapContainer, options);
+
+            const markerImage = new kakao.maps.MarkerImage(
+              "/image/marker.png",
+              new kakao.maps.Size(44, 47),
+              { offset: new kakao.maps.Point(22, 47) }
+            );
+            const markerPosition = new kakao.maps.LatLng(
               mapValue.position.lat,
               mapValue.position.lng
-            ),
-          };
-          const kakaoMap = new window.kakao.maps.Map(mapContainer, options);
+            );
+            const marker = new kakao.maps.Marker({
+              position: markerPosition,
+              image: markerImage,
+            });
 
-          const markerImage = new kakao.maps.MarkerImage(
-            "/image/marker.png",
-            new kakao.maps.Size(44, 47),
-            { offset: new kakao.maps.Point(22, 47) }
-          );
-          const markerPosition = new kakao.maps.LatLng(
-            mapValue.position.lat,
-            mapValue.position.lng
-          );
-          const marker = new kakao.maps.Marker({
-            position: markerPosition,
-            image: markerImage,
-          });
+            marker.setMap(kakaoMap);
+            kakaoMap.addControl(
+              new kakao.maps.ZoomControl(),
+              kakao.maps.ControlPosition.RIGHT
+            );
 
-          marker.setMap(kakaoMap);
-          kakaoMap.addControl(
-            new kakao.maps.ZoomControl(),
-            kakao.maps.ControlPosition.RIGHT
-          );
-
-          const content = `
+            const content = `
               <div class="speech-bubble">
                 <span class="text">${mapValue.name}</span>
                 <svg class="arrow" width="20" height="20" xmlns="http://www.w3.org/2000/svg">
@@ -61,20 +63,22 @@
               </div>
             `;
 
-          new kakao.maps.CustomOverlay({
-            map: kakaoMap,
-            position: new kakao.maps.LatLng(
-              mapValue.position.lat,
-              mapValue.position.lng
-            ),
-            content,
-            yAnchor: 1,
+            new kakao.maps.CustomOverlay({
+              map: kakaoMap,
+              position: new kakao.maps.LatLng(
+                mapValue.position.lat,
+                mapValue.position.lng
+              ),
+              content,
+              yAnchor: 1,
+            });
           });
-        });
-      };
-      mapScript.addEventListener("load", onLoadKakaoMap);
-    })
+        };
+        mapScript.addEventListener("load", onLoadKakaoMap);
+      }))
   );
+
+  onDestroy(unsubscribeMap);
 </script>
 
 {#if $map}
